a1d7a3905b7f686767e114c2df64ef9f67ef76c0
arcommit <73856634+arcommit@users.noreply.github.com>
2021-02-26 00:13:50 +0000

    Update hangman_words.py

--- hangman_words.py
+++ hangman_words.py
@@ -1,7 +1,13 @@
-word_list = ('ant baboon bat bear camel cat cobra '
-             'coyote crow deer dog donkey duck eagle fox frog goat '
-             'goose hawk lion lizard llama mole monkey mouse mule '
-             'otter owl panda parrot pigeon python rabbit ram rat raven '
-             'rhino salmon seal shark sheep skunk sloth snake spider '
-             'swan tiger toad turkey turtle weasel whale wolf '
-             'zebra ').split()
+word_list = ('Squirrel Dog Chimpanzee Ox Lion '
+              'Panda Walrus Otter Mouse '
+              'Kangaroo Goat Horse Monkey Cow Koala '
+              'Mole Elephant Leopard Hippopotamus Giraffe Fox '
+              'Coyote Hedgehong Sheep Deer ').split()
+
+#word_list = ('ant baboon bat bear camel cat cobra '
+#             'coyote crow deer dog donkey duck eagle fox frog goat '
+#             'goose hawk lion lizard llama mole monkey mouse mule '
+#             'otter owl panda parrot pigeon python rabbit ram rat raven '
+#             'rhino salmon seal shark sheep skunk sloth snake spider '
+#             'swan tiger toad turkey turtle weasel whale wolf '
+#             'zebra ').split()
